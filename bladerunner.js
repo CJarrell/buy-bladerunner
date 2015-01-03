56a865e1db7c2b1479657797337d710a8aace062
Colby Jarrell <colby.jarrell@icloud.com>
2015-01-02 23:39:48 -0500

    Removed home screen
    Home screen seemed excessive so I made it so the user can input name
    and class on “.screen-who”.

--- bladerunner.js
+++ bladerunner.js
@@ -9,30 +9,25 @@ $(function(){
   });
 
   $(".go-to-who").on("click",function(){
+    $(".screen-work").hide();
+    $(".screen-shop").hide();
     $(".screen-title").fadeOut("slow", function(){
       $(".screen-who").fadeIn();
     }); 
   });
-
-  $(".go-to-home").on("click",function(){
-    $(".screen-who").hide();
-    $(".screen-shop").hide();
-    $(".screen-work").hide();
-    $(".screen-home").show();
-  });
   
   $(".go-to-work").on("click",function(){
-    $(".screen-home").hide();
+    $(".screen-who").hide();
     $(".screen-work").show();
   });
 
   $(".go-to-shop").on("click",function(){
-    $(".screen-home").hide();
+    $(".screen-who").hide();
     $(".screen-shop").show();
   });
 
   $(".go-to-title").on("click",function(){
-    $(".screen-home").hide();
+    $(".screen-who").hide();
     $(".screen-title").show();
   });
 
