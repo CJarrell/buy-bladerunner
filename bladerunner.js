335e8224dd38e690150b6140b530768dafcca914
Colby Jarrell <colby.jarrell@icloud.com>
2015-01-06 19:08:47 -0500

    More typewriter fixing

--- bladerunner.js
+++ bladerunner.js
@@ -32,76 +32,78 @@ $(function(){
   });
 
   //Typewriter Effect Plugin
+
+  (function ($) {
+    //writes the string
+    function typeString($target, str, cursor, delay, cb) {
+      $target.html(function (_, html) {
+        return html + str[cursor];
+      });
+
+      if(cursor < str.length - 1) {
+        setTimeout(function () {
+          typeString($target, str, cursor + 1, delay, cb);
+        }, delay);
+      }
+      else {
+        cb();
+      }
+    }
+    //clears the string
+    function deleteString($target, delay, cb) {
+      var length;
+
+      $target.html(function (_, html) {
+        length = html.length;
+        return html.substr(0, length - 1);
+      });
+
+      if (length > 1) {
+        setTimeout(function () {
+          deleteString($target, delay, cb);
+        }, delay);
+      }
+      else {
+        cb();
+      }
+    }
+    //jQuery hook
+    $.fn.extend({
+      teletype: function (opts) {
+        var settings = $.extend({}, $.teletype.defaults, opts);
+
+        return $(this).each(function () {
+          (function loop($tar, idx) {
+            //type
+            typeString($tar, settings.text[idx], 0, settings.delay, function () {
+              //delete
+              setTimeout(function () {
+                deleteString($tar, settings.delay, function () {
+                  loop($tar, (idx + 1) % settings.text.length);
+                });
+              }, settings.pause);
+            });
+
+          }($(this), 0));
+        });
+      }
+    });
+
+    //plugin defaults
+    $.extend({
+      teletype: {
+        defaults: {
+          delay: 100,
+          pause: 5000,
+          text: []
+        }
+      }
+    });
+  }(jQuery));
+
   $(".click-here").click(function () {
-    (function ($) {
-      //writes the string
-      function typeString($target, str, cursor, delay, cb) {
-        $target.html(function (_, html) {
-          return html + str[cursor];
-        });
-
-        if(cursor < str.length - 1) {
-          setTimeout(function () {
-            typeString($target, str, cursor + 1, delay, cb);
-          }, delay);
-        }
-        else {
-          cb();
-        }
-      }
-      //clears the string
-      function deleteString($target, delay, cb) {
-        var length;
-
-        $target.html(function (_, html) {
-          length = html.length;
-          return html.substr(0, length - 1);
-        });
-
-        if (length > 1) {
-          setTimeout(function () {
-            deleteString($target, delay, cb);
-          }, delay);
-        }
-        else {
-          cb();
-        }
-      }
-      //jQuery hook
-      $.fn.extend({
-        teletype: function (opts) {
-          var settings = $.extend({}, $.teletype.defaults, opts);
-
-          return $(this).each(function () {
-            (function loop($tar, idx) {
-              //type
-              typeString($tar, settings.text[idx], 0, settings.delay, function () {
-                //delete
-                setTimeout(function () {
-                  deleteString($tar, settings.delay, function () {
-                    loop($tar, (idx + 1) % settings.text.length);
-                  });
-                }, settings.pause);
-              });
-
-            }($(this), 0));
-          });
-        }
-      });
-
-      //plugin defaults
-      $.extend({
-        teletype: {
-          defaults: {
-            delay: 100,
-            pause: 5000,
-            text: []
-          }
-        }
-      });
-    }(jQuery));
-
-    //typewriter effect init
+    $(".click-here").hide();
+  //typewriter effect init
     $('#target').teletype({
       text: [
       'Welcome to the work screen. Here you can earn Chin-Yen to take to the shop.'
